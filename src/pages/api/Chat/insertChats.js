233460c4eca775eb9017368dcc1b11b1338d7272
Irving <idairreyes@gmail.com>
2023-08-20 23:55:56 -0500

    arreglo de importaciones de chat

--- src/pages/api/Chat/insertChats.js
+++ src/pages/api/Chat/insertChats.js
@@ -1,54 +1,54 @@
-import User from "models/user";
-import { getServerSession } from "next-auth";
-import { connectDB } from "utils/mongoose";
-import { authOptions } from "../auth/[...nextauth]";
-connectDB();
-export default async function handler(req, res) {
-  const { method } = req;
-  const { messages, email } = req.body;
-  const session = await getServerSession(req, res, authOptions );
+// import User from "models/user";
+// import { getServerSession } from "next-auth";
+// import { connectDB } from "utils/mongoose";
+// import { authOptions } from "../auth/[...nextauth]";
+// connectDB();
+// export default async function handler(req, res) {
+//   const { method } = req;
+//   const { messages, email } = req.body;
+//   const session = await getServerSession(req, res, authOptions );
   
-  switch (method) {
-    case "GET":
-      try {
-        if(!session) return res.status(401).json({message: "Unauthorized"});
-        const {chats} = await User.findOne({ email: req.query.email }).select("chats");
-        return res.status(200).json(chats);
-      } catch (error) {
-        console.log(error);
-        return res.status(500).json({ error: error.message });
-      }
+//   switch (method) {
+//     case "GET":
+//       try {
+//         if(!session) return res.status(401).json({message: "Unauthorized"});
+//         const {chats} = await User.findOne({ email: req.query.email }).select("chats");
+//         return res.status(200).json(chats);
+//       } catch (error) {
+//         console.log(error);
+//         return res.status(500).json({ error: error.message });
+//       }
 
-    case "POST":
-      try {
+//     case "POST":
+//       try {
  
-        // const user = await User.findOneAndUpdate({email: session.user.email}, {$push: {chats: req.body}}, {new: true})
-        const datos = {
-          role: messages.role,
-          content: messages.content,
-        };
-        const user = await User.findOneAndUpdate(
-          { email: email },
-          { $push: { chats: datos } },
-          { new: true }
-        );
+//         // const user = await User.findOneAndUpdate({email: session.user.email}, {$push: {chats: req.body}}, {new: true})
+//         const datos = {
+//           role: messages.role,
+//           content: messages.content,
+//         };
+//         const user = await User.findOneAndUpdate(
+//           { email: email },
+//           { $push: { chats: datos } },
+//           { new: true }
+//         );
 
-        return res.status(200).json(user);
-      } catch (error) {
-        console.log(error);
-        return res.status(500).json({ error: error.message });
-      }
-    case "DELETE":
-      try {
-        const user = await User.findOneAndUpdate(
-          { email: req.query.email },
-          { chats: [] },
-          { new: true }
-        );
-        const chat = user.chats;
-        return res.status(200).json(chat);
-      } catch (error) {
-        console.log(error);
-      }
-  }
-}
+//         return res.status(200).json(user);
+//       } catch (error) {
+//         console.log(error);
+//         return res.status(500).json({ error: error.message });
+//       }
+//     case "DELETE":
+//       try {
+//         const user = await User.findOneAndUpdate(
+//           { email: req.query.email },
+//           { chats: [] },
+//           { new: true }
+//         );
+//         const chat = user.chats;
+//         return res.status(200).json(chat);
+//       } catch (error) {
+//         console.log(error);
+//       }
+//   }
+// }
--- src/pages/api/chat.ts
+++ src/pages/api/chat.ts
@@ -1,39 +1,39 @@
-import { Configuration, OpenAIApi } from "openai-edge";
+// import { Configuration, OpenAIApi } from "openai-edge";
 
-import { OpenAIStream, StreamingTextResponse } from "ai";
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-  organization: process.env.OPENAI_ORG,
-});
+// import { OpenAIStream, StreamingTextResponse } from "ai";
+// const configuration = new Configuration({
+//   apiKey: process.env.OPENAI_API_KEY,
+//   organization: process.env.OPENAI_ORG,
+// });
 
-const openai = new OpenAIApi(configuration);
-export const config = {
-  runtime: "edge",
-};
+// const openai = new OpenAIApi(configuration);
+// export const config = {
+//   runtime: "edge",
+// };
 
-export default async function handler(req:Request, res:Response) {
-  const method = req.method;
+// export default async function handler(req:Request, res:Response) {
+//   const method = req.method;
 
-  if (method === "GET") {
-  }
-  if (method === "POST") {
-    const { messages } = await req.json();
+//   if (method === "GET") {
+//   }
+//   if (method === "POST") {
+//     const { messages } = await req.json();
 
-    const res = await fetch("/api/products");
-    const productos = await res.json();
+//     const res = await fetch("/api/products");
+//     const productos = await res.json();
     
-    const contextMessage = {
-      role: "system",
-      content: `Eres un asistente virtual o chatbot de una tienda de venta de productos de navidad, y los que están disponibles son: ${productos.slice(0,10).map(producto => `${producto.name} (${producto.description}) - Precio: ${producto.price} - Categoría: ${producto.category} - Imagen: ${producto.image} - url:/products/${producto._id} `).join(', ')}. Solo puedes dar respuestas relacionadas con ese tema y con los productos. Si alguien hace una pregunta fuera del tema de la tienda, da un aviso de que no puedes cambiar el tema. No devuelvas links, ni urls, el resto si. No responderás a preguntas fuera de este contexto`,
-    };
-    const conversation = [contextMessage, ...messages];
+//     const contextMessage = {
+//       role: "system",
+//       content: `Eres un asistente virtual o chatbot de una tienda de venta de productos de navidad, y los que están disponibles son: ${productos.slice(0,10).map(producto => `${producto.name} (${producto.description}) - Precio: ${producto.price} - Categoría: ${producto.category} - Imagen: ${producto.image} - url:/products/${producto._id} `).join(', ')}. Solo puedes dar respuestas relacionadas con ese tema y con los productos. Si alguien hace una pregunta fuera del tema de la tienda, da un aviso de que no puedes cambiar el tema. No devuelvas links, ni urls, el resto si. No responderás a preguntas fuera de este contexto`,
+//     };
+//     const conversation = [contextMessage, ...messages];
 
-    const response = await openai.createChatCompletion({
-      model: "gpt-3.5-turbo",
-      stream: true,
-      messages:conversation,
-    });
-    const stream = OpenAIStream(response);
-    return new StreamingTextResponse(stream);
-  }
-}
+//     const response = await openai.createChatCompletion({
+//       model: "gpt-3.5-turbo",
+//       stream: true,
+//       messages:conversation,
+//     });
+//     const stream = OpenAIStream(response);
+//     return new StreamingTextResponse(stream);
+//   }
+// }
